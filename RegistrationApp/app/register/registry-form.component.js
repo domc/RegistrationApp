56cc1cb389e70e5dd59c2cd10b29db4ed718775f
Domen Perenic <domc987@gmail.com>
2017-04-25 20:56:25 +0200

    Component.ts restructured.

--- RegistrationApp/app/register/registry-form.component.js
+++ RegistrationApp/app/register/registry-form.component.js
@@ -1,90 +1,113 @@
-angular.
-    module('registryForm').
-    component('registryForm', {
-    templateUrl: 'app/register/registry-form.template.html',
-    controller: ['Register', '$indexedDB', "$filter", function RegistryFormController(Applicant, $indexedDB, $filter) {
-            var self = this;
-            //Max date value for datepicker.
-            self.maxDate = new Date();
-            //Registered user(s) retrieved from indexedDB, empty by default
-            self.registeredApplicant = [];
-            //Form submission
-            self.saveApplicant = function (formValidationCheck) {
-                if (formValidationCheck) {
-                    var applicantsAge = self.getAge(this.DateOfBirth);
-                    if (applicantsAge >= 21) {
-                        //Prepare data for db
-                        var parsedDate = $filter('date')(this.DateOfBirth, "yyyy-MM-dd");
-                        var newApplicant_1 = new Applicant({
-                            Name: this.Name,
-                            LastName: this.LastName,
-                            Address: this.Address,
-                            DateOfBirth: parsedDate
-                        });
-                        //Save data to local MYSQL database
-                        Applicant.save(newApplicant_1, function (applicantResponse, ResponseHeaders) {
-                            //Save data to indexedDB after the applicant is successfully stored in DB
-                            var isDobFriday = applicantResponse.isDOBFriday;
-                            var DateOfCreation = new Date();
-                            self.saveToIndexedDB(newApplicant_1, DateOfCreation, isDobFriday);
-                        }, function () {
-                            self.deleteDataFromIndexedDB();
-                        });
+var app;
+(function (app) {
+    var components;
+    (function (components) {
+        "use strict";
+        var RegistryFormController = (function () {
+            function RegistryFormController(Applicant, $indexedDB, $filter) {
+                this.saveApplicant = function (formValidationCheck) {
+                    var _this = this;
+                    if (formValidationCheck) {
+                        var applicantsAge = this.getAge(this.DateOfBirth);
+                        if (applicantsAge >= 21) {
+                            //Prepare data for db
+                            var parsedDate = this.$filter('date')(this.DateOfBirth, "yyyy-MM-dd");
+                            var newApplicant_1 = new this.Applicant({
+                                Name: this.Name,
+                                LastName: this.LastName,
+                                Address: this.Address,
+                                DateOfBirth: parsedDate
+                            });
+                            //Save data to local MYSQL database
+                            this.Applicant.save(newApplicant_1, function (applicantResponse, ResponseHeaders) {
+                                //Save data to indexedDB after the applicant is successfully stored in DB
+                                var isDobFriday = applicantResponse.isDOBFriday;
+                                var DateOfCreation = new Date();
+                                _this.saveToIndexedDB(newApplicant_1, DateOfCreation, isDobFriday);
+                            }, function () {
+                                _this.deleteDataFromIndexedDB();
+                            });
+                        }
+                        else {
+                            //If applicant is under 21 years old, deny registration.
+                            alert("You cannot register at this site!");
+                            this.deleteDataFromIndexedDB();
+                        }
                     }
                     else {
-                        //If applicant is under 21 years old, deny registration.
-                        alert("You cannot register at this site!");
-                        self.deleteDataFromIndexedDB();
+                        this.deleteDataFromIndexedDB();
                     }
-                }
-                else {
-                    self.deleteDataFromIndexedDB();
-                }
-            };
-            self.getAge = function (dateOfBirth) {
-                var today = new Date();
-                var age = today.getFullYear() - dateOfBirth.getFullYear();
-                var monthdifference = today.getMonth() - dateOfBirth.getMonth();
-                if (monthdifference < 0) {
-                    age--;
-                }
-                else if (monthdifference == 0) {
-                    if (today.getDate() - dateOfBirth.getDate() < 0) {
+                };
+                this.getAge = function (dateOfBirth) {
+                    var today = new Date();
+                    var age = today.getFullYear() - dateOfBirth.getFullYear();
+                    var monthdifference = today.getMonth() - dateOfBirth.getMonth();
+                    if (monthdifference < 0) {
                         age--;
                     }
-                }
-                return age;
-            };
-            //Saving applicants data in indexedDB
-            self.saveToIndexedDB = function (applicant, DateOfCreation, isDobFriday) {
-                $indexedDB.openStore('applicants', function (store) {
-                    store.clear();
-                    store.insert(applicant);
-                    store.insert({ "DateOfCreation": DateOfCreation });
-                    store.insert({ "isDobFriday": isDobFriday });
-                    store.getAll().then(function (registeredUsers) {
-                        self.registeredApplicant = registeredUsers;
+                    else if (monthdifference == 0) {
+                        if (today.getDate() - dateOfBirth.getDate() < 0) {
+                            age--;
+                        }
+                    }
+                    return age;
+                };
+                this.saveToIndexedDB = function (applicant, DateOfCreation, isDobFriday) {
+                    var _this = this;
+                    this.$indexedDB.openStore('applicants', function (store) {
+                        store.clear();
+                        store.insert(applicant);
+                        store.insert({ "DateOfCreation": DateOfCreation });
+                        store.insert({ "isDobFriday": isDobFriday });
+                        store.getAll().then(function (registeredUsers) {
+                            _this.registeredApplicant = registeredUsers;
+                        });
                     });
-                });
-            };
-            //Get applicant's data from indexedDB
-            self.getDataFromIndexedDB = function () {
-                $indexedDB.openStore('applicants', function (store) {
-                    store.getAll().then(function (registeredUsers) {
-                        self.registeredApplicant = registeredUsers;
+                };
+                //Get applicant's data from indexedDB
+                this.getDataFromIndexedDB = function () {
+                    var _this = this;
+                    this.$indexedDB.openStore('applicants', function (store) {
+                        store.getAll().then(function (registeredUsers) {
+                            _this.registeredApplicant = registeredUsers;
+                        });
                     });
-                });
-            };
-            //Check for data in indexedDB
-            this.getDataFromIndexedDB();
-            //Delete applicant currently saved in indexedDB
-            self.deleteDataFromIndexedDB = function () {
-                $indexedDB.openStore('applicants', function (store) {
-                    store.clear().then(function () {
-                        self.registeredApplicant = [];
+                };
+                //Delete applicant currently saved in indexedDB
+                this.deleteDataFromIndexedDB = function () {
+                    var _this = this;
+                    this.$indexedDB.openStore('applicants', function (store) {
+                        store.clear().then(function () {
+                            _this.registeredApplicant = [];
+                        });
                     });
-                });
-            };
-        }]
-});
+                };
+                this.Applicant = Applicant;
+                this.$filter = $filter;
+                this.$indexedDB = $indexedDB;
+                //Max date value for datepicker.
+                this.maxDate = new Date();
+                //Registered user(s) retrieved from indexedDB, empty by default
+                this.registeredApplicant = [];
+                //Check for data in indexedDB
+                this.getDataFromIndexedDB();
+            }
+            RegistryFormController.$inject = [
+                'Register',
+                '$indexedDB',
+                "$filter"
+            ];
+            return RegistryFormController;
+        }());
+        components.RegistryFormController = RegistryFormController;
+        var RegistryForm = (function () {
+            function RegistryForm() {
+                this.controller = RegistryFormController;
+                this.templateUrl = 'app/register/registry-form.template.html';
+            }
+            return RegistryForm;
+        }());
+        angular.module('registryForm').component('registryForm', new RegistryForm());
+    })(components = app.components || (app.components = {}));
+})(app || (app = {}));
 //# sourceMappingURL=registry-form.component.js.map
--- RegistrationApp/app/register/registry-form.component.ts
+++ RegistrationApp/app/register/registry-form.component.ts
@@ -1,100 +1,127 @@
-﻿angular.
-    module('registryForm').
-    component('registryForm', {
-        templateUrl: 'app/register/registry-form.template.html',
-        controller: ['Register', '$indexedDB', "$filter", function RegistryFormController(Applicant: IApplicantResource, $indexedDB, $filter) {
-            var self = this;
+﻿module app.components {
+    "use strict";
+
+    export class RegistryFormController {
+    	public maxDate: Date;
+        public registeredApplicant: any;
+        private Applicant: IApplicantResource;
+        private $filter: ng.IFilterProvider;
+        private $indexedDB: any;
+
+    	public static $inject = [
+            'Register',
+            '$indexedDB',
+            "$filter"
+        ];
+
+        constructor(Applicant: IApplicantResource, $indexedDB, $filter: ng.IFilterProvider) {
+            this.Applicant = Applicant;
+            this.$filter = $filter;
+            this.$indexedDB = $indexedDB;
 
             //Max date value for datepicker.
-            self.maxDate = new Date();
+            this.maxDate = new Date();
 
             //Registered user(s) retrieved from indexedDB, empty by default
-            self.registeredApplicant = [];
+            this.registeredApplicant = [];
 
-
-            //Form submission
-            self.saveApplicant = function(formValidationCheck: boolean): void {
-                if (formValidationCheck) {
-                    let applicantsAge = self.getAge(this.DateOfBirth);
-                    if (applicantsAge >= 21) {
-                        //Prepare data for db
-                        let parsedDate = $filter('date')(this.DateOfBirth, "yyyy-MM-dd");
-
-                        let newApplicant: IApplicant = new Applicant({
-                            Name: this.Name,
-                            LastName: this.LastName,
-                            Address: this.Address,
-                            DateOfBirth: parsedDate
-                        });
-
-                        //Save data to local MYSQL database
-                        Applicant.save(newApplicant, (applicantResponse, ResponseHeaders) => {
-                            //Save data to indexedDB after the applicant is successfully stored in DB
-                            let isDobFriday = applicantResponse.isDOBFriday;
-                            let DateOfCreation = new Date();
-                            self.saveToIndexedDB(newApplicant, DateOfCreation, isDobFriday);
-                        }, () => {
-                            self.deleteDataFromIndexedDB();
-                        });
-                    }
-                    else {
-                        //If applicant is under 21 years old, deny registration.
-                        alert("You cannot register at this site!");
-                        self.deleteDataFromIndexedDB();
-                    }
-                }
-                else {
-                    self.deleteDataFromIndexedDB();
-                }
-            }
-
-            self.getAge = function(dateOfBirth: Date): number {
-                var today = new Date();
-                var age = today.getFullYear() - dateOfBirth.getFullYear();
-                var monthdifference = today.getMonth() - dateOfBirth.getMonth();
-                if (monthdifference < 0) {
-                    age--;
-                }
-                else if (monthdifference == 0) {
-                    if (today.getDate() - dateOfBirth.getDate() < 0) {
-                        age--;
-                    }
-                }
-                return age;
-            }
-
-            //Saving applicants data in indexedDB
-            self.saveToIndexedDB = function(applicant: IApplicant, DateOfCreation: Date, isDobFriday: boolean): void {
-                $indexedDB.openStore('applicants', (store) => {
-                    store.clear();
-                    store.insert(applicant);
-                    store.insert({ "DateOfCreation": DateOfCreation });
-                    store.insert({ "isDobFriday": isDobFriday });
-
-                    store.getAll().then( (registeredUsers) => {
-                        self.registeredApplicant = registeredUsers;
-                    });
-                });
-            }
-
-            //Get applicant's data from indexedDB
-            self.getDataFromIndexedDB = function(): void {
-                $indexedDB.openStore('applicants', (store) => {
-                    store.getAll().then( (registeredUsers) => {
-                        self.registeredApplicant = registeredUsers;
-                    });
-                });
-            }
             //Check for data in indexedDB
             this.getDataFromIndexedDB();
+        }
 
-            //Delete applicant currently saved in indexedDB
-            self.deleteDataFromIndexedDB = function(): void {
-                $indexedDB.openStore('applicants', (store) => {
-                    store.clear().then( () => {
-                        self.registeredApplicant = [];
+        public saveApplicant = function (formValidationCheck: boolean): void {
+            if (formValidationCheck) {
+                let applicantsAge = this.getAge(this.DateOfBirth);
+                if (applicantsAge >= 21) {
+                    //Prepare data for db
+                    let parsedDate = this.$filter('date')(this.DateOfBirth, "yyyy-MM-dd");
+
+                    let newApplicant: IApplicant = new this.Applicant({
+                        Name: this.Name,
+                        LastName: this.LastName,
+                        Address: this.Address,
+                        DateOfBirth: parsedDate
                     });
-                });
+
+                    //Save data to local MYSQL database
+                    this.Applicant.save(newApplicant, (applicantResponse, ResponseHeaders) => {
+                        //Save data to indexedDB after the applicant is successfully stored in DB
+                        let isDobFriday = applicantResponse.isDOBFriday;
+                        let DateOfCreation = new Date();
+                        this.saveToIndexedDB(newApplicant, DateOfCreation, isDobFriday);
+                    }, () => {
+                        this.deleteDataFromIndexedDB();
+                    });
+                }
+                else {
+                    //If applicant is under 21 years old, deny registration.
+                    alert("You cannot register at this site!");
+                    this.deleteDataFromIndexedDB();
+                }
             }
-        }]
-    });
+            else {
+                this.deleteDataFromIndexedDB();
+            }
+        }
+
+        private getAge = function (dateOfBirth: Date): number {
+            var today = new Date();
+            var age = today.getFullYear() - dateOfBirth.getFullYear();
+            var monthdifference = today.getMonth() - dateOfBirth.getMonth();
+            if (monthdifference < 0) {
+                age--;
+            }
+            else if (monthdifference == 0) {
+                if (today.getDate() - dateOfBirth.getDate() < 0) {
+                    age--;
+                }
+            }
+            return age;
+        }
+
+        private saveToIndexedDB = function (applicant: IApplicant, DateOfCreation: Date, isDobFriday: boolean): void {
+            this.$indexedDB.openStore('applicants', (store) => {
+                store.clear();
+                store.insert(applicant);
+                store.insert({ "DateOfCreation": DateOfCreation });
+                store.insert({ "isDobFriday": isDobFriday });
+
+                store.getAll().then((registeredUsers) => {
+                    this.registeredApplicant = registeredUsers;
+                });
+            });
+        }
+
+        //Get applicant's data from indexedDB
+        private getDataFromIndexedDB = function (): void {
+            this.$indexedDB.openStore('applicants', (store) => {
+                store.getAll().then((registeredUsers) => {
+                    this.registeredApplicant = registeredUsers;
+                });
+            });
+        }
+
+        //Delete applicant currently saved in indexedDB
+        public deleteDataFromIndexedDB = function (): void {
+            this.$indexedDB.openStore('applicants', (store) => {
+                store.clear().then(() => {
+                    this.registeredApplicant = [];
+                });
+            });
+        }
+    }
+
+    class RegistryForm implements ng.IComponentOptions {
+
+        public controller: any;
+        public templateUrl: string;
+
+        constructor() {
+            this.controller = RegistryFormController;
+            this.templateUrl = 'app/register/registry-form.template.html';
+        }
+
+    }
+
+    angular.module('registryForm').component('registryForm', new RegistryForm());
+}
