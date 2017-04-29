fc5fa43098ec1890bad65f9637d9d3a5204b6921
Domen Perenic <domc987@gmail.com>
2017-04-29 19:51:51 +0200

    More cleanup.

--- RegistrationApp/spec/registry-form.component.spec.js
+++ RegistrationApp/spec/registry-form.component.spec.js
@@ -1,27 +1,4 @@
-﻿//let ctrl = require("../app/register/registry-form.component");
-
-// First argument to 'describe' (which is defined by Jasmine) is the testing module that will
-// appear in test reports. The second argument is a callback containing the individual tests.
-
-/*describe("registryFormControllerFunctions", function () {
-    // The 'it' function of Jasmine defined an individual test. The first argument is
-    // a description of the test that's appended to the module name. Because a module name
-    // is typically a noun, like the name of the function being tested, the description for
-    // an individual test is typically written in an action-data format. 
-
-    it("calculates age from DoB", function () {
-        // Invoke the unit being tested as necessary
-        var DoB = '2007-01-01';
-        //var result = ctrl.getAge(DoB);
-        var result = getAge(DoB);
-
-        // Check the results; "expect" and toEqual are Jasmine methods.
-        expect(result).toEqual(10);
-        done();
-    });
-});*/
-
-// Suite
+﻿// Suite
 describe('Testing RegistryFormController', function () {   
     beforeEach(module('RegisterApp'));
     beforeEach(module('registryForm'));
@@ -30,8 +7,8 @@ describe('Testing RegistryFormController', function () {
     var RegisterService;
     var filter;
 
+    // Preparation for the tests
     beforeEach(inject(function ($componentController, $injector, $filter) {
-        // The injector unwraps the underscores (_) from around the parameter names when matching
         $controller = $componentController('registryForm');
         RegisterService = $injector.get('Register');
         filter=$filter("date");
@@ -40,14 +17,9 @@ describe('Testing RegistryFormController', function () {
     // Test (spec)
     describe('should calculate age', function () {
         it('returns apllicants age', function () {
-            //var controller = $controller('RegistryFormController', { Applicant: registerService.IApplicantResource, $indexedDB: indexedDB, $filter: filter });
             var DoB = '2007-01-01';
             var result = $controller.getAge(DoB);
             expect(result).toEqual(10);
-
-            DoB = '2005-01-01';
-            result = $controller.getAge(DoB);
-            expect(result).toEqual(12);
         });
     });
 });
